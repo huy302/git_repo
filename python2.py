f93a9783591b0d4d75bfe32c78e31b0ffc31bee8
Huy Nguyen <anhhuy@gmail.com>
2019-06-20 23:28:52 -0500

    add sub

--- python2.py
+++ python2.py
@@ -7,8 +7,13 @@ def sum(x, y):
 def mul(x, y):
     return x * y
 
+def sub(x, y):
+    return x - y
+
 print(f'Square of 2: {square(2)}')
 
 print(f'Sum of 2 and 3: {sum(2, 3)}')
 
 print(f'Mul of 4 and 5: {mul(4, 5)}')
+
+print(f'Sub of 4 and 5: {sub(4, 5)}')
