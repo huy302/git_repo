8a4268fb6308add289e4691f21eb77cd24912d84
Huy Nguyen <anhhuy@gmail.com>
2019-06-20 23:20:11 -0500

    add mul

--- python2.py
+++ python2.py
@@ -4,6 +4,11 @@ def square(x):
 def sum(x, y):
     return x + y
 
+def mul(x, y):
+    return x * y
+
 print(f'Square of 2: {square(2)}')
 
 print(f'Sum of 2 and 3: {sum(2, 3)}')
+
+print(f'Mul of 4 and 5: {mul(4, 5)}')
